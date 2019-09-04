e9e8be73578028dae014679321473a266e851c30
sayuri-udemy <sayuri.udemy@gmail.com>
2019-09-04 17:19:30 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

---
+++ src/main/java/com/sayuri/cursomc/services/EmailService.java
@@ -0,0 +1,13 @@
+package com.sayuri.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.sayuri.cursomc.domain.Pedido;
+
+public interface EmailService {
+	
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+	
+}
